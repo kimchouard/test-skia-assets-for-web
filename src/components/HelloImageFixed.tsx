1d438aedf0b49949704fb98ff878776a3d095ad6
Kim CHOUARD <github@chouard.kim>
2024-02-15 15:05:15 +0100

    Documenting research 🤓

--- src/components/HelloImageFixed.tsx
+++ src/components/HelloImageFixed.tsx
@@ -1,48 +1,67 @@
 import { Canvas, Image, useImage } from "@shopify/react-native-skia";
 import { Platform } from "react-native";
+import { Image as RNImage } from "react-native";
 import { useAssets } from 'expo-asset';
 import { Asset } from 'expo-asset';
 
-// Sync download image (erroring)
-async function downloadImage(assetId:number) {
-  const myImage = Asset.fromModule(assetId);
-  const image = await myImage.downloadAsync();
-  console.log('sync image', image, assetId);
-  return image;
-}
- 
 const HelloImageFixed = () => {
+  // ==============================
+  //    Require Initi
+  // ==============================
+  
+  // Publically accessible path
+  // const myAssetPath = 'assets/images/favicon.png';
+  // const tempPath = '../../';
 
-  // Ternary based on platform (preferred)
-  const myAssetPath = 'assets/images/favicon.png';
-  const asset = (Platform.OS === 'web') ? `/${myAssetPath}` : require(`../../${myAssetPath}`);
+  // Private path
+  // http://localhost:8081/assets/?unstable_path=.%2Fsrc%2Frandom-folder/favicon.png?platform=ios&hash=4f1cb2cac2370cd5050681232e8575a8
+  const myAssetPath = 'random-folder/favicon.png';
+  const myAssetAbsolutePath = '/src/random-folder/images/favicon.png';
+  const tempPath = '../';
+
+  // Getting the require
+  const assetRequire = require(`${tempPath}${myAssetPath}`);
 
   // ==============================
-  //    Other options explored
+  //    Loading Options
   // ==============================
+
+  // ONLY FOR PUBLIC FOLDER: Ternary based on platform
+  // const asset = (Platform.OS === 'web') ? `/${myAssetPath}` : assetRequire;
+
+  // PREFERED: Expo
+  const [assets, error] = useAssets([assetRequire]);
+  const asset = (!error && assets) ? assets[0].uri : null;
+
+  // ERRORING options below ==========
+
+  // RN resolveAssetSource (erroring on Web, only accessible in Native)
+  // const asset = RNImage.resolveAssetSource(assetRequire).uri;
 
   // Basic Require (erroring)
-  // const asset = require("../../assets/images/favicon.png");
+  // const asset = assetRequire;
   // console.log('Loading asset using require', asset);
-
-  // Expo
-  // const [assets, error] = useAssets([require("../../assets/images/favicon.png")]);
-  // const asset = (!error && assets) ? assets[0].uri : null;
   
   // (other option)
-  // const myImage = Asset.fromModule(require("../../assets/images/favicon.png"));
+  // const myImage = Asset.fromModule(assetRequire);
   // myImage.downloadAsync().then((image) => {
   //   console.log('image', image);
   // });
 
-  // Sync (erroring)
-  // const asset = downloadImage(require("../../assets/images/favicon.png"))
-  // console.log('Asset:', asset);
-
+  // Sync download image (erroring)
+  // async function downloadImage(assetId:number) {
+  //   const myImage = Asset.fromModule(assetId);
+  //   const image = await myImage.downloadAsync();
+  //   console.log('sync image', image, assetId);
+  //   return image;
+  // }
+  // const asset = downloadImage(assetRequire)
+  
   // ==============================
   //        useImage
   // ==============================
-
+  
+  console.log('Asset:', asset, assetRequire);
   const image = useImage(asset);
 
   return (
